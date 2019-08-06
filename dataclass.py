4d3fd7650214b11e7ed7417db565f7e96fd310a7
ytu-hakan-aksoy <hakan.aksoy@ytu-hakan-aksoys-MacBook-Pro.local>
2019-08-06 10:17:59 +0300

    data class

--- dataclass.py
+++ dataclass.py
@@ -4,7 +4,7 @@ dict2 = {
     "name":"Hakan",
     "age":22,
     "height":174,
-    # "color":"dark blue",
+    #"color":"dark blue",
     # "hobby":"table tennis",
     # "number":42
     }
@@ -14,14 +14,24 @@ class InventoryItem:
     name: str
     age: int
     height: int = 0
+
+    def name_test(self):
+        if len(self.name)<6:
+            print("Eksik kelime")
+    
+    def control(self):
+        if(self.age>18 and self.height>170):
+            print("Yeterli")
+        elif(self.name!="Hakan"):
+            print("Yanlis kelime")
+        else: pass
+    
     
 test = InventoryItem(**dict2)
 print(test)
 print(dict2)
-
-
-
-
+test.name_test()
+test.control()
 
 # TEST DATACLASS
 # @dataclass
