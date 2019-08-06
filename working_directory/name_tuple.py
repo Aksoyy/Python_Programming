4909fb5256a481a94ff074fd7b51b34fa4317979
ytu-hakan-aksoy <hakan.aksoy@ytu-hakan-aksoys-MacBook-Pro.local>
2019-08-06 11:48:45 +0300

    name_tuple

--- working_directory/name_tuple.py
+++ working_directory/name_tuple.py
@@ -1,21 +1,18 @@
 from collections import namedtuple
 
-NodeT = namedtuple('Node', 'val left right')
-def Node(val, left=None, right=None):
-  return NodeT(val, left, right)
-
+#DEFAULT PARAMETERS EXAM
 Color = namedtuple('Color', ['r', 'g', 'b', 'a'])
 Color.__new__.__defaults__ = (0.0, 0.0, 0.0, 1.0)
-# Default RGBA values for black
 
 RED = Color(r=1.0)
 GREEN = Color(g=1.0)
 BLUE = Color(b=1.0)
-BLACK = Color()
-print(RED)
+BLACK = Color() 
+print(RED,GREEN,BLACK)
 
 
 import collections
+
 #EXAM
 def namedtuple_with_defaults(typename, fields_dict):
     T = collections.namedtuple(typename, ' '.join(fields_dict.keys()))
